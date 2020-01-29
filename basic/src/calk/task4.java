3c9c8bf191ceff46a673da9d1a2a8bcafa70428d
Mutsuki-Moromi <m2_069_ihy@yahoo.co.jp>
2020-01-29 20:54:40 +0900

    課題：java基礎：四則演算：task4

---
+++ basic/src/calk/task4.java
@@ -0,0 +1,12 @@
+package calk;
+
+public class task4 {
+
+	public static void main(String[] args) {
+		int a = 192837;
+		
+		System.out.println("一の位の数を除いた数:" + ( a /= 10 ) );
+		System.out.println("一の位の数:" + ( a %= 1928370 ));
+	}
+
+}
