6c024efbd3d91156068246ef313c509465a1b02f
Mutsuki-Moromi <m2_069_ihy@yahoo.co.jp>
2020-01-31 20:14:52 +0900

    課題：javaの基礎：四則演算：課題５

---
+++ basic/src/calk/task5.java
@@ -0,0 +1,20 @@
+package calk;
+
+import java.util.Scanner;
+
+public class task5 {
+
+	public static void main(String[] args) {
+		String a = "ノートPCの値段:";
+		long b = 89800L;
+		char c = '円';
+
+		System.out.println( a + b + c );
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+
+	}
+
+}
